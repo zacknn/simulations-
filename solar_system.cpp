a401f801fb7fda1ad810f2b297cd0de00e2603da
zaknx7 <zakitalhi13@gmail.com>
2026-05-20 23:58:24 +0100

    try to make the class planet

--- solar_system.cpp
+++ solar_system.cpp
@@ -1,25 +1,45 @@
 #include <SFML/Graphics.hpp>
-#include <cmath>
-#include <iostream>
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Shape.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <string>
-#include <vector>
 
-int main () {
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "solar system",
+struct Plannet {
+  std::string name;
+  float mass;
+  sf::Vector2f position;
+  sf::Vector2f velocity;
+
+  sf::CircleShape Circle;
+
+  Plannet(std::string n, float m, float radius, sf::Vector2f pos,
+          sf::Vector2f vel, sf::Color color)
+      : name(n), mass(m), position(pos), velocity(vel) {
+    Circle.setRadius(radius);
+    Circle.setFillColor(color);
+    Circle.setPosition(position);
+    Circle.setOrigin(position);
+  }
+};
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "solar system",
                           sf::Style::Titlebar | sf::Style::Close);
-    window.setFramerateLimit(60);
+  window.setFramerateLimit(60);
+  Plannet sun("Sun", 1.989e30f, 40.0f, sf::Vector2f(640, 360),
+              sf::Vector2f(0, 0), sf::Color::Yellow);
 
-
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-            }
-
-        window.clear(sf::Color::Black);
-        window.display();
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>())
+        window.close();
     }
-    return 0;
+
+    window.clear();
+    window.draw(sun.Circle);
+    window.display();
+  }
+
+  return 0;
 }
