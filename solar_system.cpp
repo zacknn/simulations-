08adc80a3344bffe7a98964f7558c428383614b6
zaknx7 <zakitalhi13@gmail.com>
2026-05-21 00:32:36 +0100

    still trying to figure out how to make the movmeent corect

--- solar_system.cpp
+++ solar_system.cpp
@@ -2,7 +2,9 @@
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Shape.hpp>
+#include <SFML/System/Clock.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <SFML/Window/Window.hpp>
 #include <string>
 
 struct Plannet {
@@ -18,26 +20,47 @@ struct Plannet {
       : name(n), mass(m), position(pos), velocity(vel) {
     Circle.setRadius(radius);
     Circle.setFillColor(color);
+    Circle.setOrigin({radius, radius});
     Circle.setPosition(position);
-    Circle.setOrigin(position);
+  }
+  void update(float dt, const Plannet &other) {
+    sf::Vector2f dir = other.position - position;
+
+    float dist = std::sqrt(dir.x * dir.x + dir.y * dir.y);
+
+    sf::Vector2f dirNorm = dir / dist;
+
+    float G = 1000.f;
+    float accel = G * other.mass / (dist * dist);
+
+    velocity += dirNorm * accel * dt;
+    position += velocity * dt;
+    Circle.setPosition(position);
   }
 };
 
 int main() {
   sf::RenderWindow window(sf::VideoMode({800, 600}), "solar system",
                           sf::Style::Titlebar | sf::Style::Close);
+  sf::Vector2f center = sf::Vector2f(window.getSize()) / 2.f;
   window.setFramerateLimit(60);
-  Plannet sun("Sun", 1.989e30f, 40.0f, sf::Vector2f(640, 360),
-              sf::Vector2f(0, 0), sf::Color::Yellow);
-
+  Plannet sun("Sun", 1.0e6f, 40.0f, sf::Vector2f(center), sf::Vector2f(0, 0),
+              sf::Color::Yellow);
+  Plannet earth("Earth", 1.0f, 10.0f, sf::Vector2f(640, 160),
+                sf::Vector2f(15.0f, 0.0f), sf::Color::Blue);
+  sf::Clock clock;
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
       if (event->is<sf::Event::Closed>())
         window.close();
     }
+    float dt = clock.restart().asSeconds();
+
+    earth.update(dt, sun);
 
     window.clear();
     window.draw(sun.Circle);
+    window.draw(earth.Circle);
     window.display();
   }
 
